672d3dd364b34644cf573d249c9ea999ca0927b3
Vishal Sanserwal <vsanserwal@sapient.com>
2019-11-21 23:59:07 +0530

    minor update

--- src/Components/Atoms/Button.js
+++ src/Components/Atoms/Button.js
@@ -5,7 +5,7 @@ export default function Button(props) {
     btnClass = props.isLoading?`${btnClass} isloading`: btnClass;
     return (
         <button name={props.btnName || ""} onClick={props.handleClick} className={btnClass} title={props.btnTitle} type={props.btnType}  >
-            <span>{props.isLoading?<p className="loading">Please wait<span>.</span><span>.</span><span>.</span></p>: props.btnLabel}</span>
+            <>{props.isLoading?<p className="loading">Please wait<span>.</span><span>.</span><span>.</span></p>: props.btnLabel}</>
         </button>
     );
 }
