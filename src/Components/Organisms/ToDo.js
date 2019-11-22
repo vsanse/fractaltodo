c605ff0c6b9185ad9a7c376552dec8c51db2168b
Vishal Sanserwal <vsanserwal@sapient.com>
2019-11-22 11:19:54 +0530

    click area fix

--- src/Components/Organisms/ToDo.js
+++ src/Components/Organisms/ToDo.js
@@ -6,7 +6,8 @@ export default function ToDo(props) {
     const cardRef = useRef();
     const handleActiveBucket = (event) => {
         if (event.target.classList.contains('fa') ||
-            event.target.classList.contains('todo-bucket-heading')) {
+            event.target.classList.contains('todo-bucket-heading')|| 
+            event.target.classList.contains('todo-bucket-title')) {
             getClosestParent(event.target, ".card").classList.toggle("active")
         }
 
